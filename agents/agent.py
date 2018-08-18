841aa1c5073be7ed342b7b863bb8ea7ae15c3b0b
Yoshi Kato <yoshi.samurai+gh@gmail.com>
2018-08-17 23:35:53 -0700

    Wrap up

--- agents/agent.py
+++ agents/agent.py
@@ -1,7 +1,10 @@
-from agents.actor import Actor
-from agents.critic import Critic
-from utils import ReplayBuffer, OUNoise
+# TODO: your agent here!
+import copy
+import random
 import numpy as np
+from keras import layers, models, optimizers
+from keras import backend as K
+from collections import namedtuple, deque
 
 class DDPG():
     """Reinforcement Learning agent that learns using DDPG."""
@@ -27,7 +30,7 @@ class DDPG():
         # Noise process
         self.exploration_mu = 0
         self.exploration_theta = 0.15
-        self.exploration_sigma = 0.2
+        self.exploration_sigma = 0.2 * 2
         self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
 
         # Replay memory
@@ -38,14 +41,29 @@ class DDPG():
         # Algorithm parameters
         self.gamma = 0.99  # discount factor
         self.tau = 0.01  # for soft update of target parameters
+        
+        # Score
+        self.total_reward = 0 # sum of reward among one episode
+        self.count = 0 # step count
+        self.score = 0 # total_reward / count
+        self.best_score = -np.inf # best score among entier episodes
 
     def reset_episode(self):
+        # Reset score params
+        self.total_reward = 0
+        self.count = 0
+        self.score = 0
+        
         self.noise.reset()
         state = self.task.reset()
         self.last_state = state
         return state
 
     def step(self, action, reward, next_state, done):
+        # Increment score params
+        self.count += 1
+        self.total_reward += reward
+        
          # Save experience / reward
         self.memory.add(self.last_state, action, reward, next_state, done)
 
@@ -53,6 +71,11 @@ class DDPG():
         if len(self.memory) > self.batch_size:
             experiences = self.memory.sample()
             self.learn(experiences)
+            
+        # Average reward as score
+        if done:
+            self.score = self.total_reward / float(self.count) if self.count > 0 else 0
+            self.best_score = max(self.best_score, self.score)
 
         # Roll over last state and action
         self.last_state = next_state
@@ -87,7 +110,7 @@ class DDPG():
 
         # Soft-update target models
         self.soft_update(self.critic_local.model, self.critic_target.model)
-        self.soft_update(self.actor_local.model, self.actor_target.model)
+        self.soft_update(self.actor_local.model, self.actor_target.model)   
 
     def soft_update(self, local_model, target_model):
         """Soft update model parameters."""
@@ -98,3 +121,169 @@ class DDPG():
 
         new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
         target_model.set_weights(new_weights)
+        
+class Actor:
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, action_low, action_high):
+        """Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            action_low (array): Min value of each action dimension
+            action_high (array): Max value of each action dimension
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.action_low = action_low
+        self.action_high = action_high
+        self.action_range = self.action_high - self.action_low
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build an actor (policy) network that maps states -> actions."""
+        # Define input layer (states)
+        states = layers.Input(shape=(self.state_size,), name='states')
+
+        # Add hidden layers
+        net = layers.Dense(units=32, activation='relu')(states)
+        net = layers.Dense(units=64, activation='relu')(net)
+        net = layers.Dense(units=32, activation='relu')(net)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+
+        # Add final output layer with sigmoid activation
+        raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',
+            name='raw_actions')(net)
+
+        # Scale [0, 1] output for each action dimension to proper range
+        actions = layers.Lambda(lambda x: (x * self.action_range) + self.action_low,
+            name='actions')(raw_actions)
+
+        # Create Keras model
+        self.model = models.Model(inputs=states, outputs=actions)
+
+        # Define loss function using action value (Q value) gradients
+        action_gradients = layers.Input(shape=(self.action_size,))
+        loss = K.mean(-action_gradients * actions)
+
+        # Incorporate any additional losses here (e.g. from regularizers)
+
+        # Define optimizer and training function
+        optimizer = optimizers.Adam()
+        updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
+        self.train_fn = K.function(
+            inputs=[self.model.input, action_gradients, K.learning_phase()],
+            outputs=[],
+            updates=updates_op)
+        
+class Critic:
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size):
+        """Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        # Define input layers
+        states = layers.Input(shape=(self.state_size,), name='states')
+        actions = layers.Input(shape=(self.action_size,), name='actions')
+
+        # Add hidden layer(s) for state pathway
+        net_states = layers.Dense(units=32, activation='relu')(states)
+        net_states = layers.Dense(units=64, activation='relu')(net_states)
+
+        # Add hidden layer(s) for action pathway
+        net_actions = layers.Dense(units=32, activation='relu')(actions)
+        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+
+        # Try different layer sizes, activations, add batch normalization, regularizers, etc.
+
+        # Combine state and action pathways
+        net = layers.Add()([net_states, net_actions])
+        net = layers.Activation('relu')(net)
+
+        # Add more layers to the combined network if needed
+
+        # Add final output layer to prduce action values (Q values)
+        Q_values = layers.Dense(units=1, name='q_values')(net)
+
+        # Create Keras model
+        self.model = models.Model(inputs=[states, actions], outputs=Q_values)
+
+        # Define optimizer and compile model for training with built-in loss function
+        optimizer = optimizers.Adam()
+        self.model.compile(optimizer=optimizer, loss='mse')
+
+        # Compute action gradients (derivative of Q values w.r.t. to actions)
+        action_gradients = K.gradients(Q_values, actions)
+
+        # Define an additional function to fetch action gradients (to be used by actor model)
+        self.get_action_gradients = K.function(
+            inputs=[*self.model.input, K.learning_phase()],
+            outputs=action_gradients)
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size: maximum size of buffer
+            batch_size: size of each training batch
+        """
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self, batch_size=64):
+        """Randomly sample a batch of experiences from memory."""
+        return random.sample(self.memory, k=self.batch_size)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+    
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state    
--- task.py
+++ task.py
@@ -1,6 +1,82 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
+class HoverTask():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+
+        print(init_pose)
+        print(self.target_pos)
+
+    # x2
+    def get_reward(self):
+        distance = (self.sim.pose[:3] - self.target_pos) * [0.3, 0.3, 1]  # emphasizes the z-distance
+        distance = 1 / (np.linalg.norm(distance) + 1)
+
+        #velocity_z = np.tanh(self.sim.v[2]) * 2 # -0.5 ~ 0.5
+        #reward = 0.8 * distance + 0.2 * velocity_z
+
+        reward = 2 * distance - 1
+        return reward / self.action_repeat, -1, 1
+
+    # distance + z_velocity
+    def get_reward_distance_vz(self):
+        # x,y,Z distance
+        distance = (self.sim.pose[:3] - self.target_pos) * [0.2, 0.2, 1]  # emphasizes the z-distance
+        distance = 1 / (np.linalg.norm(distance) + 1) # 0~1.0
+        distance = 2 * distance - 1.0 # -1~1.0
+
+        # z velocity to be (+)
+        v_z = 2 * np.tanh(self.sim.v[2]) # -1~1
+
+        # sum
+        reward = 0.8 * distance + 0.2 + v_z
+
+        return reward / self.action_repeat
+
+
+    def get_reward_x1(self):
+        """Uses current pose of sim to return reward."""
+        # reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+
+        distance = (self.sim.pose[:3] - self.target_pos) * [0.2, 0.2, 1]  # emphasizes the z-distance
+        distance = 1 / (np.linalg.norm(distance) + 1)
+        reward = 2 * distance - 1.0
+
+        reward += np.tanh(self.sim.v[2])
+        reward = np.clip(reward, -1, 1)
+        return reward / self.action_repeat
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
+
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None, 
@@ -28,6 +104,7 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
+
         reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
         return reward
 
